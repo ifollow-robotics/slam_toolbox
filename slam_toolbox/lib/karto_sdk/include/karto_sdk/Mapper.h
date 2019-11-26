c664ae612077f81ac590747a814c96fb90b86edf
stevemacenski <stevenmacenski@gmail.com>
2019-11-26 11:33:44 -0800

    clearing last and running scans for reset of state in localization mode

--- slam_toolbox/lib/karto_sdk/include/karto_sdk/Mapper.h
+++ slam_toolbox/lib/karto_sdk/include/karto_sdk/Mapper.h
@@ -1595,6 +1595,12 @@ namespace karto
     void ClearLastScan(LocalizedRangeScan* pScan);
 
     /**
+     * Clears the laser scan of device name
+     * @param pScan
+     */
+    void ClearLastScan(const Name& name);
+
+    /**
      * Gets the scan with the given unique id
      * @param id
      * @return scan
